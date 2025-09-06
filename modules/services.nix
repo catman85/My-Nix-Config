a79ba8d69941dbb60ce5b47e23683a5a435eb192
jim@desktop <jim@desktop.com>
2025-09-06 22:21:57 +0300

    fixup

--- modules/services.nix
+++ modules/services.nix
@@ -10,7 +10,7 @@
   services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
-  services.xserver.xkb = {s
+  services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
