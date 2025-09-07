585c339979213b1af14b72123b9cad652fbc973a
jim@desktop <jim@desktop.com>
2025-09-08 02:11:03 +0300

    apps install

--- modules/l14/programs.nix
+++ modules/l14/programs.nix
@@ -2,15 +2,19 @@
 
 {
 
+  environment.systemPackages = with pkgs; [
+    lm_sensors
+  ];
+
   programs.steam = {
-	enable = true;
-	remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-	dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
   };
 
   # GameMode is a daemon/lib combo for Linux that allows games to request a set of optimisations be temporarily applied to the host OS and/or a game process.
   # type inside steam game properties gamemoderun %command%
   programs.gamemode.enable = true;
 
-programs.virt-manager.enable = true;
+  programs.virt-manager.enable = true;
 }
