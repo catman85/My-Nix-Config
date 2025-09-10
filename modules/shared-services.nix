d8b8fcf33447596993e5b8fe4ca5aa7214946ee3
Dimitris Vasil <catman85@users.noreply.github.com>
2025-09-11 02:13:37 +0300

    flatpak alteration

--- modules/shared-services.nix
+++ modules/shared-services.nix
@@ -1,17 +1,31 @@
 { config, pkgs, ... }:
 
+let
+  nix-flatpak = import (builtins.fetchTarball "https://github.com/gmodena/nix-flatpak/archive/main.tar.gz") {};
+in
 {
+  imports = [
+    nix-flatpak.nixosModules.nix-flatpak
+  ];
+
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
   services.blueman.enable = true;
 
   services.flatpak.enable = true;
-  systemd.services.flatpak-repo = {
-    wantedBy = [ "multi-user.target" ];
-    path = [ pkgs.flatpak ];
-    script = ''
-      flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-    '';
-  };
+  # systemd.services.flatpak-repo = {
+  #   wantedBy = [ "multi-user.target" ];
+  #   path = [ pkgs.flatpak ];
+  #   script = ''
+  #     flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+  #   '';
+  # };
+  services.flatpak.remotes = [
+    { name = "flathub"; location = "https://dl.flathub.org/repo/flathub.flatpakrepo"; }
+  ];
+  services.flatpak.packages = [
+    "com.simplenote.Simplenote"
+    "com.viber.Viber"
+  ];
 }
