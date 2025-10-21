b9a4be51b5468e8702524bd47eee9c13ec8478b0
Dimitris Vasil <catman85@users.noreply.github.com>
2025-10-21 16:00:27 +0300

    claude desktop on desktop

--- flake.nix
+++ flake.nix
@@ -5,12 +5,14 @@
     # NixOS official package source, using the nixos-25.05 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nix-flatpak.url = "github:gmodena/nix-flatpak"; 
+    claude-desktop.url = "github:k3d3/claude-desktop-linux-flake";
   };
 
   outputs = { self, nixpkgs, nix-flatpak, ... }@inputs: {
     nixosConfigurations = {
       desktop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux"; # or your system architecture
+        specialArgs = { inherit inputs; }; 
         modules = [
           nix-flatpak.nixosModules.nix-flatpak
           ./hosts/desktop.nix
--- modules/desktop/programs.nix
+++ modules/desktop/programs.nix
@@ -1,7 +1,12 @@
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
+
+let
+  claude-desktop-fhs = inputs.claude-desktop.packages.${pkgs.system}.claude-desktop-with-fhs;
+in
 
 {
   environment.systemPackages = with pkgs; [
+    claude-desktop-fhs 
     lm_sensors
   ];
 }
